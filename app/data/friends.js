21e7e43e7acbda1ec00d8b363625f6a380b56f6b
Ibad Aslam <Ibad Aslam>
2017-04-23 16:57:20 -0400

    updating code

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var list = [
+  {
+    name: "Bob",
+    photo: "http://angus-doc.readthedocs.io/en/latest/_images/aurelien.jpg",
+    customerID: "bob123",
+    scores: ["5", "2", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+    {
+    name: "John",
+    photo: "https://i.ytimg.com/vi/qh7LLydY8eo/maxresdefault.jpg",
+    customerID: "john123",
+    scores: ["2", "1", "2", "3", "5", "2", "2", "5", "4", "4"]
+  },
+    {
+    name: "Obama",
+    photo: "http://pngimg.com/uploads/face/face_PNG5660.png",
+    customerID: "obama123",
+    scores: ["5", "1", "1", "4", "3", "5", "2", "1", "3", "5"]
+  },
+    {
+    name: "Trump",
+    photo: "https://media3.s-nbcnews.com/j/newscms/2016_20/1541946/160518-trump-portrait-jsw-145p_1c226e6636be4572928409c157f0d50a.nbcnews-ux-2880-1000.jpg",
+    customerID: "trump991",
+    scores: ["2", "4", "3", "2", "5", "1", "2", "5", "4", "1"]
+  },
+    {
+    name: "Eminem",
+    photo: "http://68.media.tumblr.com/499bd955c9d6107237696b78b1d8fd9d/tumblr_metb5qMXph1qf2qx7o1_1280.jpg",
+    customerID: "eminem12321",
+    scores: ["2", "3", "1", "5", "5", "1", "2", "3", "3", "2"]
+  }
+];
+
+module.exports = list;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,63 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var data = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(data);
+  });
+
+
+  app.post("/api/friends", function(req, res) {
+    //this is what will be displayed on the page
+    var display = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    var userData = req.body.scores;
+    var differenceNum = 0;
+
+      console.log(req.body.scores);
+       console.log(data);
+
+       //Loops through friends list
+      for (var i = 0; i < data.length; i++) {
+        differenceNum = 0;
+        for (var x = 0; x < data[i].scores.length; x++) {
+
+        differenceNum += Math.abs(parseInt(userData[x]) - parseInt(data[i].scores[x]));
+        console.log(differenceNum);
+        }
+
+       if (differenceNum <= display.friendDifference) {
+            display.name = data[i].name,
+            display.photo = data[i].photo,
+            display.friendDifference = differenceNum;
+          }
+
+       }
+       console.log(display);
+      data.push(req.body);
+
+      return res.json(display);
+
+  });
+
+
+};//end
